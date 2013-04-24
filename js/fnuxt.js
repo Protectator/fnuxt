7fa72b0bfac4b1f1874f4b703ad3d7d531b32e81
Kewin Dousse <kewin.d@websud.ch>
2013-04-24 16:59:10 +0200

    Corrected bugs with special chars

--- js/fnuxt.js
+++ js/fnuxt.js
@@ -9,7 +9,7 @@
 }
 
 function fnuText(t) {
-	var words = t.split(/([\s+|'|,|\.|!|?])/);
+	var words = t.split(/([\s+|'|,|\.|!|\?|\-|\\|\/|\|])/);
 	for (var i = 0; i < words.length; i++) {
 		var w = words[i];
 		if (/^[a-zA-Z0-9]*$/.test(w)) {words[i] = fnuWord(w);}
@@ -18,7 +18,8 @@ function fnuText(t) {
 }
 
 function isVow(ch) {
-	return /[aeiouyAEIOUYâêîôûÂÊÎÔÛàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝäëïöüÿÄËÏÖÜŸØøÆæåÅÅÃÕãõĀāĒēĂăĔĕĨĩĬĭŎŏŐőŒœŨũŬŭŮůŰűŶŷǾǿǼǽȀȁȂȃȄȅȆȇȈȉȊȋȌȍȎȏȔȕȖȗΑΕΗΙΟΥΩαεηιουωАЕИОУЭЮЯаеиоуэюяӔӕ]/.test(ch);
+	var accents = "aeiouyAEIOUYâêîôûÂÊÎÔÛàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝäëïöüÿÄËÏÖÜŸØøÆæåÅÅÃÕãõĀāĒēĂăĔĕĨĩĬĭŎŏŐőŒœŨũŬŭŮůŰűŶŷǾǿǼǽȀȁȂȃȄȅȆȇȈȉȊȋȌȍȎȏȔȕȖȗΑΕΗΙΟΥΩαεηιουωАЕИОУЭЮЯаеиоуэюяӔӕ";
+	return accents.indexOf(ch) !== -1;
 }
 
 $("#fnuxtTitle").on("mouseover", function(e){$("#fnuxtTitle").html("Fnxtu");});
