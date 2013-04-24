882a934893da766ea02e7690d26f5d8a7a2e40e7
Kewin Dousse <kewin.d@websud.ch>
2013-04-24 13:27:30 +0200

    Started html, css and js
    No bindings yet

---
+++ js/fnuxt.js
@@ -0,0 +1,21 @@
+﻿function fnuWord(w) {
+	var vow = "";
+	var con = "";
+	for (var i = 0; i < w.length; i++) {
+		var l = w.charAt(i);
+		if (isVow(l)) {vow  = vow+l;} else {con = con+l;}
+	}
+	return con+vow;
+}
+
+function fnuText(t) {
+	var words = t.split(/(\s+)/);
+	for (var i = 0; i < w.length; i++) {
+		var w = words[i];
+		if (/^[a-zA-Z]*$/.test(w)) {w = fnuWord(w)}
+	}
+}
+
+function isVow(ch) {
+	return /[aeiouyAEIOUYâêîôûÂÊÎÔÛàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝäëïöüÿÄËÏÖÜŸØøÆæåÅÅÃÕãõĀāĒēĂăĔĕĨĩĬĭŎŏŐőŒœŨũŬŭŮůŰűŶŷǾǿǼǽȀȁȂȃȄȅȆȇȈȉȊȋȌȍȎȏȔȕȖȗΑΕΗΙΟΥΩαεηιουωАЕИОУЭЮЯаеиоуэюяӔӕ]/.test(ch);
+}
